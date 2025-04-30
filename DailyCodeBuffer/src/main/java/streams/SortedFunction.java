e3db19d82c77fe9e1bf132464a06639c3614826c
Suyash Baoney <suyashbaoney58@gmail.com>
2025-04-30 20:49:13 +0530

    sorted() method

--- DailyCodeBuffer/src/main/java/streams/SortedFunction.java
+++ DailyCodeBuffer/src/main/java/streams/SortedFunction.java
@@ -1,4 +1,18 @@
 package streams;
+//sorting using sorted() method
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SortedFunction {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(23,4,2,5,7,4);
+        List newnumbers = numbers.stream()
+                .map(i-> i*i) //lambda function
+                .collect(Collectors.toList());
+        System.out.println(newnumbers);
+
+        numbers.stream().sorted().forEach(System.out::println);
+        newnumbers.stream().sorted().forEach(System.out::println);
+    }
 }
