476a412c64f4852d9532486081b3407d7eea640c
Suyash Baoney <suyashbaoney58@gmail.com>
2025-07-24 23:50:19 +0530

    Set Matrix Zeroes
    Given an array of integers arr[], the task is to find the first equilibrium point in the array.
    
    The equilibrium point in an array is an index (0-based indexing) such that the sum of all elements before that index is the same as the sum of elements after it. Return -1 if no such point exists.
    
    Examples:
    
    Input: arr[] = [1, 2, 0, 3]
    Output: 2
    Explanation: The sum of left of index 2 is 1 + 2 = 3 and sum on right of index 2 is 3.
    
    Input: arr[] = [1, 1, 1, 1]
    Output: -1
    Explanation: There is no equilibrium index in the array.
    
    Input: arr[] = [-7, 1, 5, 2, -4, 3, 0]
    Output: 3
    Explanation: The sum of left of index 3 is -7 + 1 + 5 = -1 and sum on right of index 3 is -4 + 3 + 0 = -1.
    
    Constraints:
    3 <= arr.size() <= 105
    -104 <= arr[i] <= 104

---
+++ DailyCodeBuffer/src/main/java/gfg_160/Day40_SetMatrixZeroes.java
@@ -0,0 +1,27 @@
+package gfg_160;
+/*
+You are given a 2D matrix mat[][] of size n x m. The task is to modify the matrix such that if mat[i][j] is 0, all the elements in the i-th row and j-th column are set to 0 and do it in constant space complexity.
+ */
+public class Day40_SetMatrixZeroes {
+    public static void main(String[] args) {
+
+    }
+    public static void setMatrixZeroes(int[][] mat) {
+        // code here
+        int n=mat.length, m=mat[0].length, col0=1;
+        for(int i=0; i<n; i++){
+            if(mat[i][0]==0)
+                col0=0;
+            for(int j=1; j<m;j++)
+                if(mat[i][j]==0)
+                    mat[i][0]=mat[0][j]=0;
+        }
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=1;j--)
+                if(mat[i][0]==0||mat[0][j]==0)
+                    mat[i][j]=0;
+            if(col0==0)
+                mat[i][0]=0;
+        }
+    }
+}
