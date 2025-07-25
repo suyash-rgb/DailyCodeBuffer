cc549ad59887a73414f718b390ef8cdfb8b247e0
Suyash Baoney <suyashbaoney58@gmail.com>
2025-07-25 15:18:35 +0530

    Count Pairs with Given Sum
    Given an array arr[] and an integer target. You have to find numbers of pairs in array arr[] which sums up to given target.
    
    Examples:
    
    Input: arr[] = [1, 5, 7, -1, 5], target = 6
    Output: 3
    Explanation: Pairs with sum 6 are (1, 5), (7, -1) and (1, 5).
    Input: arr[] = [1, 1, 1, 1], target = 2
    Output: 6
    Explanation: Pairs with sum 2 are (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1).
    Input: arr[] = [10, 12, 10, 15, -1], target = 125
    Output: 0
    Constraints:
    1 <= arr.size() <= 105
    -104 <= arr[i] <= 104
    1 <= target <= 104

---
+++ DailyCodeBuffer/src/main/java/gfg_160/Day43_CountPairsWithGivenSum.java
@@ -0,0 +1,38 @@
+package gfg_160;
+
+import java.util.HashMap;
+
+/*
+Given an array arr[] and an integer target. You have to find numbers of pairs in array arr[] which sums up to given target.
+
+Examples:
+
+Input: arr[] = [1, 5, 7, -1, 5], target = 6
+Output: 3
+Explanation: Pairs with sum 6 are (1, 5), (7, -1) and (1, 5).
+Input: arr[] = [1, 1, 1, 1], target = 2
+Output: 6
+Explanation: Pairs with sum 2 are (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1).
+Input: arr[] = [10, 12, 10, 15, -1], target = 125
+Output: 0
+Constraints:
+1 <= arr.size() <= 105
+-104 <= arr[i] <= 104
+1 <= target <= 104
+ */
+public class Day43_CountPairsWithGivenSum {
+    public static void main(String[] args) {
+
+    }
+    int countPairs(int[] arr, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+
+        for (int num : arr) {
+            count += map.getOrDefault(target - num, 0);
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        return count;
+    }
+}
