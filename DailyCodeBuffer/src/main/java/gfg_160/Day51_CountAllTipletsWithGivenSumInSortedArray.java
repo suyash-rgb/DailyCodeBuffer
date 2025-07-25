bbd6dd0482890df7c02af9391c09fc025f053645
Suyash Baoney <suyashbaoney58@gmail.com>
2025-07-25 16:12:15 +0530

    Count all triplets with given sum in sorted array(incomplete)
    Given a sorted array arr[] and a target value, the task is to count triplets (i, j, k) of valid indices, such that arr[i] + arr[j] + arr[k] = target and i < j < k.
    
    Examples:
    
    Input: arr[] = [-3, -1, -1, 0, 1, 2], target = -2
    Output: 4
    Explanation: Four triplets that add up to -2 are:
    arr[0] + arr[3] + arr[4] = (-3) + 0 + (1) = -2
    arr[0] + arr[1] + arr[5] = (-3) + (-1) + (2) = -2
    arr[0] + arr[2] + arr[5] = (-3) + (-1) + (2) = -2
    arr[1] + arr[2] + arr[3] = (-1) + (-1) + (0) = -2
    Input: arr[] = [-2, 0, 1, 1, 5], target = 1
    Output: 0
    Explanation: There is no triplet whose sum is equal to 1.
    Constraints:
    3 ≤ arr.size() ≤ 104
    -105 ≤ arr[i], target ≤ 105

---
+++ DailyCodeBuffer/src/main/java/gfg_160/Day51_CountAllTipletsWithGivenSumInSortedArray.java
@@ -0,0 +1,28 @@
+package gfg_160;
+/*
+Given a sorted array arr[] and a target value, the task is to count triplets (i, j, k) of valid indices, such that arr[i] + arr[j] + arr[k] = target and i < j < k.
+
+Examples:
+
+Input: arr[] = [-3, -1, -1, 0, 1, 2], target = -2
+Output: 4
+Explanation: Four triplets that add up to -2 are:
+arr[0] + arr[3] + arr[4] = (-3) + 0 + (1) = -2
+arr[0] + arr[1] + arr[5] = (-3) + (-1) + (2) = -2
+arr[0] + arr[2] + arr[5] = (-3) + (-1) + (2) = -2
+arr[1] + arr[2] + arr[3] = (-1) + (-1) + (0) = -2
+
+Input: arr[] = [-2, 0, 1, 1, 5], target = 1
+Output: 0
+Explanation: There is no triplet whose sum is equal to 1.
+
+Constraints:
+3 ≤ arr.size() ≤ 104
+-105 ≤ arr[i], target ≤ 105
+ */
+public class Day51_CountAllTipletsWithGivenSumInSortedArray {
+    public static void main(String[] args) {
+
+    }
+
+}
